b05d10eb275397dc53caa34da55a558d4e66f94f
Lalit <gangwarlalitk@gmail.com>
2025-05-25 19:12:18 +0530

    Added dockerfile

--- api/index.py
+++ api/index.py
@@ -51,5 +51,5 @@ def guessUser():
 def guessComp():
     return render_template('guessComp.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
